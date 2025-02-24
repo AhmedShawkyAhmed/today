3f88654acdae585c49efedc7abb60b228a826a54
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2025-02-24 20:33:21 +0200

    init

---
+++ todayTests/todayTests.swift
@@ -0,0 +1,17 @@
+//
+//  todayTests.swift
+//  todayTests
+//
+//  Created by Ahmed Shawky on 24/02/2025.
+//
+
+import Testing
+@testable import today
+
+struct todayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
